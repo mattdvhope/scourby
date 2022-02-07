a2d79aea72d4f9383224e6ab6c22861bda23b960
Matt Malone <mattagape@me.com>
2022-02-07 12:01:45 -0500

    Changed logo

--- src/components/header.js
+++ src/components/header.js
@@ -2,14 +2,15 @@ import React from "react"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 
-import StrapiLogo from "~/images/strapi.png"
+import "../styles/global.css";
 
 const Header = ({ setOpenModal }) => {
+  const LogoImage = `https://res.cloudinary.com/dwpkckmyf/image/upload/v1644251334/Bible%20App%20Joy/bajoy-log_xicz77.jpg`;
   return (
     <header className="p-8">
       <div className="flex flex-col items-center">
         <Link className="mx-2 text-lg w-40 mb-4" to="/">
-          <img src={StrapiLogo} alt="strapi catalog logo" />
+          <img className="image-blurred-edge" src={LogoImage} alt="logo catalog logo"/>
         </Link>
         <div className="flex flex-col items-center md:flex-row">
           <Link className="mx-2 mb-2 md:mb-0 text-lg" to="/">
