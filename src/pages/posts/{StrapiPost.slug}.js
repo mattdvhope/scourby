8acf557f7fd8e63394ce815bdb3419255dfcaa86
Matt Malone <mattagape@me.com>
2022-02-11 13:59:47 -0500

    Set up posts, rather than products

--- src/pages/posts/{StrapiPost.slug}.js
+++ src/pages/posts/{StrapiPost.slug}.js
@@ -26,20 +26,9 @@ console.log(post.title)
     <Layout>
       <SEO seo={seo} />
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-4 mt-4">
-        {/*{post.image && (
-          <div className="md:col-span-2 md:pr-4">
-            <Image
-              className="rounded-md"
-              image={post.image}
-              alt="Post Image"
-            />
-          </div>
-        )}*/}
-
         <div className="md:col-span-2 md:pr-4">
           <img className="rounded-t-md border-gray-200   border-b" src={post.image} alt={post.title}/>
         </div>
-
         <h1 >{post.title}</h1>
       </div>
       <div className="my-6 mb-1">
@@ -49,16 +38,6 @@ console.log(post.title)
         />
         <hr className="mt-6 mb-12 m-auto w-24 border-t-4" />
       </div>
-      {/*{post.relatedPosts.length > 0 && (
-        <div className="flex flex-col my-6 mb-24">
-          <h2 className="text-3xl font-bold text-center">Related Posts</h2>
-          <hr className="mt-6 mb-12 m-auto w-24 border-t-4" />
-          <PostList
-            posts={post.relatedPosts}
-            gridCols="grid-cols-1 md:grid-cols-2"
-          />
-        </div>
-      )}*/}
     </Layout>
   )
 }
