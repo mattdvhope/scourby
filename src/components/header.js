fdcf11efdd84774a81a3a718e8aff2e361af5052
Matt Malone <mattagape@me.com>
2022-03-16 13:56:11 -0400

    Changed alt for img

--- src/components/header.js
+++ src/components/header.js
@@ -12,7 +12,7 @@ const Header = ({ setOpenModal, sitename, metadata }) => {
     <header className="p-8">
       <div className="flex flex-col items-center">
         <Link className="mx-2 text-lg w-40 mb-4" to="/">
-          <img className="image-blurred-edge" src={LogoImage} alt="logo catalog logo"/>
+          <img className="image-blurred-edge" src={LogoImage} alt="Bible App Joy"/>
         </Link>
       </div>
       <hr className="mt-1 m-auto w-24 border-t-4" />
@@ -36,8 +36,8 @@ const Header = ({ setOpenModal, sitename, metadata }) => {
           </div>
 
           <div className="google-play-store-image">
-            <Link
-              to="/order"
+            <a 
+              href="https://order.shareit.com/cart/add?vendorid=200281724&PRODUCT%5B300918420%5D=1&currency=USD&languageid=1&affiliateid=200292863"
               onClick={() => {
                 console.log("clicked google")
                 fbq('track', 'PageView'); // you can add JSON params here too!! --> https://developers.facebook.com/docs/mediaguide/pixel-and-analytics
@@ -48,7 +48,7 @@ const Header = ({ setOpenModal, sitename, metadata }) => {
                 alt="App Store Image"
                 image={metadata.google_play_store}
               />
-            </Link>
+            </a>
           </div>
         </div>
       </div>
