eb80b835eaeb74cd6c0f1f69c1c174bccc75228b
Matt Malone <mattagape@me.com>
2022-03-24 09:34:26 -0400

    Removed console log

--- src/components/header.js
+++ src/components/header.js
@@ -24,7 +24,6 @@ const Header = ({ setOpenModal, sitename, metadata }) => {
             <Link
               to="/order"
               onClick={() => {
-                console.log("clicked apple")
                 fbq('track', 'PageView'); // you can add JSON params here too!! --> https://developers.facebook.com/docs/mediaguide/pixel-and-analytics
               }}
             >
@@ -39,7 +38,6 @@ const Header = ({ setOpenModal, sitename, metadata }) => {
             <a 
               href="https://order.shareit.com/cart/add?vendorid=200281724&PRODUCT%5B300918420%5D=1&currency=USD&languageid=1&affiliateid=200292863"
               onClick={() => {
-                console.log("clicked google")
                 fbq('track', 'PageView'); // you can add JSON params here too!! --> https://developers.facebook.com/docs/mediaguide/pixel-and-analytics
                 // fbq('trackCustom', 'ClickedGooglePlayStore'); // you can add JSON params here too!! --> https://developers.facebook.com/docs/mediaguide/pixel-and-analytics
               }}
