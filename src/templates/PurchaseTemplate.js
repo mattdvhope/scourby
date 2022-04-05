d9e22937462dacf2e3209e5ba547a99df98ea9c4
Matt Malone <mattagape@me.com>
2022-04-05 12:40:12 -0400

    Styled Purchase Template

--- src/templates/PurchaseTemplate.js
+++ src/templates/PurchaseTemplate.js
@@ -4,23 +4,29 @@ import Layout from "~/components/layout"
 import Image from "~/components/image"
 import "~/styles/global.css";
 
-
 const PurchaseTemplate = ({edges, imageAlt}) =>
   <Layout>
-    <div className="centered-div" style={{ marginTop: `-40px` }}>
-      <Image
-        className="audio-bible-voice-only-image??????"
-        alt={imageAlt}
-        image={edges[0].node.metadata.image}
-      />
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-24 mt-4" style={{ marginTop: `-65px` }}>
       {edges.map(({node}) => {
         return (
-          <a href={node.metadata.sub_product_characteristic.product_link} key={node.id}>
-            <h3>{node.title}</h3>
-            <br/>
-            <hr/>
-            <br/>
-          </a>
+          <div className="enclosed-product">
+            <a href={node.metadata.sub_product_characteristic.product_link} key={node.id}>
+              <div className="grid md:grid-cols-2 gap-4 mb-3 mt-4">
+                <Image
+                  className="audio-bible-image"
+                  alt={imageAlt}
+                  image={edges[0].node.metadata.image}
+                />
+                <p className="product-title text-center">{node.title}</p>
+              </div>
+            </a>
+            <div
+              className="prose md:w-4/5 m-auto"
+              dangerouslySetInnerHTML={{
+                __html: node.content
+              }}
+            />
+          </div>
         )
       })}
     </div>
