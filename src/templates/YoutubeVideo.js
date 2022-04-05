9e29128c114ae34a0097fad902e9b1d4e3046e24
Matt Malone <mattagape@me.com>
2022-04-05 13:30:57 -0400

    Styling on vlog page

--- src/templates/YoutubeVideo.js
+++ src/templates/YoutubeVideo.js
@@ -3,9 +3,10 @@ import "~/styles/global.css"
 
 const YoutubeVideo = ({src, title}) => {
 	return (
-    <div>
+    <div className="youtube-video-container">
   		<iframe
         id="FrameHolder"
+        className="youtube-video-container" 
         src={src}
         title={title}
         frameBorder="0"
